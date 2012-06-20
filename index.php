70f11f3a9bb7622bb0efd4c92081371a4c3c5a38
dwilson <dwilson@cibercare.com>
2012-06-19 22:27:03 -0400

    Finished addAfterMany function

--- index.php
+++ index.php
@@ -9,5 +9,8 @@ $page = setTitle("Hello World", $page);
 $page = addScript("scripts/test.js", $page);
 $page = addStyle("styles/main.css", $page);
 
+$page = addAfterMany("div", "", "many1", "body", "", $page);
+$page = addAfterMany("div", "", "many1", "div", "many1", $page);
+$page = addAfterMany("div", "", "many2", "div", "many1", $page);
 echo $page;
 ?>
--- template.php
+++ template.php
@@ -31,6 +31,78 @@ function template($tags, $data){
     return $data;
 }
 
+function addAfterMany($elementToAdd, $options, $class, $element, $afterClass, $page){
+    /*  Searches for an element that matches the specified characteristics
+    **  and adds a div with the specified id and classes after it.  If
+    **  multiple elements match the div is added after each one.
+    **  TODO: Add matching of self-closing tags
+    */
+    $matches1 = Array();
+    $matches2 = Array();
+    $matches3 = Array();
+    $matches4 = Array();
+    if($class == "" || $class == NULL){
+        $class = "";
+    }
+    else{
+        $class = "class='" . $class . "'";
+    }
+    if($afterClass == "" || $afterClass == NULL){
+        $afterClass = "";
+    }
+    else{
+        $afterClass = "class='" . $afterClass . "'";
+    }
+    //match an opening tag of the search element with the class identifiers
+    $openWithClass = "%(<\s*" . $element . "\s*" . $afterClass . "\s*>)%";
+    //match an opening tag of the search element
+    $open = "%(<\s*" . $element . "[^>]*>)%";
+    //match a closing tag of the search element
+    $close = "%(</\s*" . $element . "\s*>)%";
+    //match the next opening tag of the search element if there is no closing tag
+    //of the search element first
+    $nextOpen = "%(<[^/]*" . $element . "[^>]*>)%";
+    //non-greedy match of a whole search element
+    $whole = "%(<\s*" . $element . "[^>]*>).*?</\s*" . $element . "\s*>%";
+
+    /* So basically what's going on here is we do a non greedy search for any element
+    ** of the specified type. As long as the HTML is well formed (and if it comes from
+    ** us it should be) this will always go for the deepest node first.  After we match
+    ** that we yank out of the tree and put it in an array to await further processing.
+    ** This way we can loop through the tree indefinitely.
+    */
+    $nodes = Array();
+    $data = $page;
+    while(preg_match($whole, $data, $matches, PREG_OFFSET_CAPTURE) > 0){
+        $data = substr_replace($data, "", $matches[0][1], strlen($matches[0][0]));
+
+        $sibling = False;
+        if(preg_match($nextOpen, $data, $matches3) > 0){
+            $sibling = True;
+        }
+        //we define the new div if the element matches our specifications
+        $add = "";
+        if(preg_match($openWithClass, $matches[0][0], $matches2) > 0){
+            $add = "<" . $elementToAdd . " " . $class . " " . $options . "></div>";
+        }
+
+        $nodes[] = Array('data' => $matches[0][0], 'add' => $add, 'pos' => $matches[0][1], 'end' => $matches[0][1] + strlen($matches[0][0]), 'sibling' => $sibling);
+    }
+    //pop the stuff off in reverse order
+    while(($node = array_pop($nodes)) != NULL) {
+        //if the node is a sibling we have to make sure it gets placed in the right order
+        //we popped the siblings off in reverse
+        $node2 = array_pop($nodes);
+        while($node2 != NULL && $node2['sibling'] == true){
+            $add = $node2['data'] . $node2['add'];
+            $node['data'] = $add . $node['data'];
+            $node2 = array_pop($nodes);
+        }
+        $data = substr_replace($data, $node['data'] . $node['add'], $node['pos'], 0);
+    }
+    return $data;
+}
+
 function addScript($script, $page){
     /*  Specific version of the templating function which adds the specified
     **  script to the bottom of the <head> section.
